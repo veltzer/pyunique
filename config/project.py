b94c5aaaa7582136e1e0518eb3a52e5bf2c01b39
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-27 00:26:58 +0300

    more stuff

--- config/project.py
+++ config/project.py
@@ -23,7 +23,7 @@ project_keywords = [
     "hash",
 ]
 project_license = "MIT"
-project_year_started = "2020"
+project_year_started = 2020
 project_description = project_short_description
 project_platforms = [
     "python3",
