401d9b7ab4e8002879331754840dc0d2857df620
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:12:40 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -3,40 +3,31 @@ import config.project
 package_name = config.project.project_name
 
 console_scripts = [
-    'pyunique=pyunique.main:main',
-]
-
-setup_requires = [
+    "pyunique=pyunique.main:main",
 ]
 
 run_requires = [
-    'pytconf',
-    'pylogconf',
-    'tqdm',
-    'lmdb',
+    "pytconf",
+    "pylogconf",
+    "tqdm",
+    "lmdb",
 ]
 
 test_requires = [
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'flake8',
-    'pymakehelper',
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "flake8",
+    "pymakehelper",
 ]
 
 dev_requires = [
-    'pyclassifiers',
-    'pypitools',
-    'pydmt',
-    'Sphinx',
-    'pyyaml',
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
+    "pyyaml",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
