47a328a835ca9ad231d6b315987d41b88af7c8a9
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 03:02:02 +0200

    fix for new pydmt

--- config/python.py
+++ config/python.py
@@ -1,29 +1,33 @@
-console_scripts = [
+from typing import List
+
+
+console_scripts: List[str] = [
     "pyunique=pyunique.main:main",
 ]
-config_requires = [
+config_requires: List[str] = [
     "pyyaml",
 ]
-dev_requires = [
+dev_requires: List[str] = [
     "pypitools",
 ]
-make_requires = [
+install_requires: List[str] = [
+    "pytconf",
+    "pylogconf",
+    "tqdm",
+    "lmdb",
+]
+make_requires: List[str] = [
     "pyclassifiers",
     "pydmt",
     "sphinx",
     "pymakehelper",
     "types-tqdm",
 ]
-install_requires = [
-    "pytconf",
-    "pylogconf",
-    "tqdm",
-    "lmdb",
-]
-test_requires = [
+test_requires: List[str] = [
     "pylint",
     "pytest",
     "pytest-cov",
     "flake8",
     "mypy",
 ]
+requires = config_requires + install_requires + make_requires + test_requires
