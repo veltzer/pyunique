11f22efe1453cf976472ae198c201ae9f4c908d8
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 21:46:25 +0300

    more

---
+++ pyunique/static.py
@@ -0,0 +1,5 @@
+""" version which can be consumed from within the module """
+VERSION_STR = "0.0.11"
+DESCRIPTION = "Pyunique helps you get rid of duplicate files"
+APP_NAME = "pyunique"
+LOGGER_NAME = "pyunique"
