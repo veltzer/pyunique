34499e7a9657ebc82a394cffbaa315ac62de4ffb
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:18 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -1,30 +1,20 @@
 """ python deps for this project """
 
-scripts: dict[str, str] = {
-    "pyunique": "pyunique.main:main",
-}
+import config.shared
 
-config_requires: list[str] = [
-    "pyclassifiers",
-]
 install_requires: list[str] = [
     "pytconf",
     "pylogconf",
     "tqdm",
     "lmdb",
 ]
-build_requires: list[str] = [
-    "hatch",
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-]
-test_requires: list[str] = [
-    "pylint",
-    "pytest",
-    "mypy",
-    "ruff",
-    # types
+build_requires: list[str] = config.shared.PBUILD
+test_requires: list[str] = config.shared.PTEST
+types_requires: list[str] = [
     "types-tqdm",
 ]
-requires = config_requires + install_requires + build_requires + test_requires
+requires = install_requires + build_requires + test_requires + types_requires
+
+scripts: dict[str, str] = {
+    "pyunique": "pyunique.main:main",
+}
